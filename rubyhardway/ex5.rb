b7d2bb0ab767c06834c12101d1f79d7d01147ede
Nelson Miranda <nmirandaghn@gmail.com>
2017-10-17 00:58:32 -0600

    Let's continue tomorrow

---
+++ rubyhardway/ex5.rb
@@ -0,0 +1,18 @@
+name = 'Nelson Miranda'
+age = 42
+height = 176 / 2.54 # centimeters
+weight = 200 / 2.20462 # pounds
+eyes = "Brown"
+teeth = "White"
+hair = "Black"
+
+puts "Let's talk abount #{name}."
+puts "He's #{height} centimeters tall."
+puts "He's #{weight} pounds heavy."
+puts "Actually, that's not too heavy."
+puts "He's got #{eyes} eyes and #{hair} hair."
+puts "His teeth are usually #{teeth} depending on the coffee."
+
+# This line is tricky, try to get it exactly right
+puts "If I add #{age}, #{height}, and #{weight} I get #{age + height + weight}."
+
